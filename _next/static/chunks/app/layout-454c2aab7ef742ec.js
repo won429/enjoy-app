53411db23fabd1b9f58526628f1a114422f083e3
박성원 <sungwon4867@naver.com>
2026-09-10 17:51:07 +0900

    Add Asian Games static runtime assets

---
+++ _next/static/chunks/app/layout-454c2aab7ef742ec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6151:()=>{},8449:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6151,23))}},e=>{e.O(0,[390,441,794,358],()=>e(e.s=8449)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/layout-c86e6d1d0986c680.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1624:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6151,23))},6151:()=>{}},e=>{e.O(0,[390,441,794,358],()=>e(e.s=1624)),_N_E=e.O()}]);
